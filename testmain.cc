9bcdb805f7c8f6576ccb0134caca626d74bc15fe
ilmazza <mrosellini@navionics.com>
2014-10-29 15:10:59 +0100

    disable test

--- testmain.cc
+++ testmain.cc
@@ -5,15 +5,15 @@ T somma(T a, T b){
 return (a+b);
 }
 
-TEST(somma, integer) {
-  EXPECT_EQ(3, somma<int>(1,2));
-  EXPECT_EQ(5, somma<int>(3,2));
-}
+//TEST(somma, integer) {
+//  EXPECT_EQ(3, somma<int>(1,2));
+//  EXPECT_EQ(5, somma<int>(3,2));
+//}
 
 
 int main(){
   std::cout << "Hello word" << std::endl;
-  RUN_ALL_TESTS();
+  //RUN_ALL_TESTS();
 return 0;
 
 }
